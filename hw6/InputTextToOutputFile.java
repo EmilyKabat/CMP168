a8c59536f8089f405a69506be3a6e1ab01417056
EmilyKabat <47428022+EmilyKabat@users.noreply.github.com>
2020-04-12 23:15:00 -0400

    Update InputTextToOutputFile.java

--- hw6/InputTextToOutputFile.java
+++ hw6/InputTextToOutputFile.java
@@ -1,43 +1,29 @@
-// package hw6;
-
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.Scanner;
 
-public class InputTextToOutputFile {
-    public static void main(String[] args) throws FileNotFoundException {
-        Scanner scnr = new Scanner(System.in);
-        String userFile = "";
-        char cont = 'y';
+public class InputTextToOutputFile{
+	 public static void main(String[] args) throws FileNotFoundException {
+			 Scanner scnr = new Scanner(System.in);
+			 String userFile = "";
+			 char cont = 'y';
 
-        System.out.println("What is the name of your output file?");
-        if (scnr.hasNext()) {
-            userFile = scnr.nextLine().replace(" ",  "");
-        }
-        try {
-            String s = "";
-            FileOutputStream fileStream = null;
-            PrintWriter outFS = null;
-
-            fileStream = new FileOutputStream(userFile);
-            outFS = new PrintWriter(fileStream);
-            s += scnr.nextLine();
-            s += "\n";
-            while (!s.substring(s.length() - 5, s.length() - 1).contentEquals("STOP")) {
-                s += scnr.nextLine();
-                s += "\n";
-            }
-            s = s.substring(0, s.length()-5);
-            System.out.println(s);
-//            outFS.println(s);
-            cont = 'n';
-            outFS.flush();
-
-            fileStream.close();
-        }
-        catch (Exception FileNotFoundException){
-            System.out.println("File Not Found " + userFile);
-        }
-    }
+			 System.out.println("What is the name of your output file?");
+			 if (scnr.hasNext()) {
+					 userFile = scnr.nextLine().replace(" ",  "");
+			 }
+			 try {
+					 String s = "";
+					 FileWriter fileWriter = new FileWriter(userFile);
+					 if (scnr.nextLine().contains("STOP!")) {
+						 s += scnr.nextLine();
+						 s += "\n";
+					 }
+					 fileWriter.write(s);
+					 fileWriter.close();
+					 cont = 'n';
+			 }
+			 catch (Exception FileNotFoundException){
+					 System.out.println("File Not Found " + userFile);
+			 }
+	 }
 }
